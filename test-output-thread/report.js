4fdb6a692dfd43be7fa3db55213d1cb719828252
Rajesh Yemul <rajesh.yemul@globant.com>
2021-07-15 12:34:48 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "bb291b83-dfb6-474f-97c6-335620d7b5e4",
+    "feature": "Login Page feature",
+    "scenario": "Login Page title validation",
+    "start": 1626332469479,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1626332484187,
+    "className": "passed"
+  },
+  {
+    "id": "e475f47a-7a85-4ec0-ab2b-0de84c6481be",
+    "feature": "Account page feature",
+    "scenario": "Verify Accounts section count",
+    "start": 1626332474390,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1626332489158,
+    "className": "failed"
+  },
+  {
+    "id": "9b014b94-72a5-4c79-ba40-24dffa99cb93",
+    "feature": "Login Page feature",
+    "scenario": "Validate with correct credentials user can login to the application",
+    "start": 1626332498284,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1626332500447,
+    "className": "failed"
+  },
+  {
+    "id": "fb768c36-fc9d-4a5c-b852-13c5d4f43ac7",
+    "feature": "Account page feature",
+    "scenario": "Verify Account page title",
+    "start": 1626332469478,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1626332474377,
+    "className": "failed"
+  },
+  {
+    "id": "1df7bc61-6c77-4f38-b04d-2045152a6d28",
+    "feature": "Login Page feature",
+    "scenario": "Validate Forgot password link is displayed",
+    "start": 1626332484193,
+    "group": 13,
+    "content": "",
+    "tags": "",
+    "end": 1626332498277,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 13,
+    "content": "Thread[pool-2-thread-1,5,main]"
+  },
+  {
+    "id": 14,
+    "content": "Thread[pool-2-thread-2,5,main]"
+  }
+]);
+});
